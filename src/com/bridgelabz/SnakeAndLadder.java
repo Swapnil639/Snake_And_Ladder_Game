b92890774170f24ccf2569d55fc03cec3aa4f236
Swapnil639 <swapnilpatil631997@gmail.com>
2023-06-22 23:19:40 +0530

    Added :Player checks for Option

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -1,11 +1,34 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
     public static void main(String[] args) {
         int playerPosition = 0;
         System.out.println("Player at start position " + playerPosition);
 
-        int randomDice = (int) (Math.floor(Math.random()*10)%6+1);
-        System.out.println("Player rolls the die to get number is "+randomDice);
+        int rollDice = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+        System.out.println("Player roll the die to get number is " + rollDice);
+
+        Random random = new Random();
+        int option = random.nextInt(3);
+        System.out.println("Check option "+option);
+
+        switch (option) {
+            case 0:
+                System.out.println("No play");
+                playerPosition+=0;
+                System.out.println("Player current position "+playerPosition);
+                break;
+            case 1:
+                playerPosition += rollDice;
+                System.out.println("Player current position "+playerPosition);
+                break;
+            case 2:
+                playerPosition -= rollDice;
+                System.out.println("Player current Position "+playerPosition);
+                break;
+        }
+
     }
 }
