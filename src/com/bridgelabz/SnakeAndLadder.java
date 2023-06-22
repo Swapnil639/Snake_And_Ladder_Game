83dfced1e7bc433ba15ca8991101c406bede9acb
Swapnil639 <swapnilpatil631997@gmail.com>
2023-06-22 23:30:45 +0530

    Added :Repeat till the Player reaches the winning position 100.

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -10,24 +10,26 @@ public class SnakeAndLadder {
         int rollDice = (int) (Math.floor(Math.random() * 10) % 6 + 1);
         System.out.println("Player roll the die to get number is " + rollDice);
 
-        Random random = new Random();
-        int option = random.nextInt(3);
-        System.out.println("Check option "+option);
+        while (playerPosition != 100) {
+            Random random = new Random();
+            int option = random.nextInt(3);
+            System.out.println("Check option " + option);
 
-        switch (option) {
-            case 0:
-                System.out.println("No play");
-                playerPosition+=0;
-                System.out.println("Player current position "+playerPosition);
-                break;
-            case 1:
-                playerPosition += rollDice;
-                System.out.println("Player current position "+playerPosition);
-                break;
-            case 2:
-                playerPosition -= rollDice;
-                System.out.println("Player current Position "+playerPosition);
-                break;
+            switch (option) {
+                case 0:
+                    System.out.println("No play");
+                    playerPosition += 0;
+                    System.out.println("Player current position " + playerPosition);
+                    break;
+                case 1:
+                    playerPosition += rollDice;
+                    System.out.println("Player current position " + playerPosition);
+                    break;
+                case 2:
+                    playerPosition -= rollDice;
+                    System.out.println("Player current Position " + playerPosition);
+                    break;
+            }
         }
 
     }
