b7e095440fda9e83a219a3e7ccbe17ca8b9ae239
Swapnil639 <swapnilpatil631997@gmail.com>
2023-06-23 00:09:43 +0530

    Added :Ensure the player gets to exact winning position 100.

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -8,9 +8,10 @@ public class SnakeAndLadder {
         System.out.println("Player at start position " + playerPosition);
 
         int rollDice = (int) (Math.floor(Math.random() * 10) % 6 + 1);
-        System.out.println("Player roll the die to get number is " + rollDice);
 
         while (playerPosition != 100) {
+            System.out.println("Player roll the die to get number is " + rollDice);
+
             Random random = new Random();
             int option = random.nextInt(3);
             System.out.println("Check option " + option);
@@ -19,15 +20,21 @@ public class SnakeAndLadder {
                 case 0:
                     System.out.println("No play");
                     playerPosition += 0;
-                    System.out.println("Player current position " + playerPosition);
+                    System.out.println("No Play - You stay in the same position");
                     break;
                 case 1:
                     playerPosition += rollDice;
-                    System.out.println("Player current position " + playerPosition);
+                    if (playerPosition > 100) {
+                        playerPosition += 0;
+                    }
+                    System.out.println("You got a ladder. Your position is now : " + playerPosition);
                     break;
                 case 2:
                     playerPosition -= rollDice;
-                    System.out.println("Player current Position " + playerPosition);
+                    if (playerPosition < 0) {
+                        playerPosition = 0;
+                    }
+                    System.out.println("You got a snake. Your position is now : " + playerPosition);
                     break;
             }
         }
