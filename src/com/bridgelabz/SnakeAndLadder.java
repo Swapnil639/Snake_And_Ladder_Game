e96eec4123bdd374e25d23e25a0d3bd324b88ac1
Swapnil639 <swapnilpatil631997@gmail.com>
2023-06-23 10:25:40 +0530

    Added :Player won the game

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -4,13 +4,19 @@ import java.util.Random;
 
 public class SnakeAndLadder {
     public static void main(String[] args) {
+        playGame();
+    }
+
+    public static void playGame() {
         int count=0;
-        int playerPosition = 0;
-        System.out.println("Player at start position " + playerPosition);
+        int player1Position = 0;
+        int player2Position = 0;
+        System.out.println("Player1 at start position " + player1Position);
+        System.out.println("Player2 at start position " + player2Position);
 
         int rollDice = (int) (Math.floor(Math.random() * 10) % 6 + 1);
 
-        while (playerPosition != 100) {
+        while (player1Position < 100 && player2Position < 100) {
             count++;
             System.out.println("Player roll the die to get number is " + rollDice);
 
@@ -21,29 +27,54 @@ public class SnakeAndLadder {
             switch (option) {
                 case 0:
                     System.out.println("No play");
-                    playerPosition += 0;
+                    player1Position += 0;
+                    player2Position += 0;
                     System.out.println("No Play - You stay in the same position");
                     break;
                 case 1:
-                    playerPosition += rollDice;
-                    if (playerPosition > 100) {
-                        playerPosition += 0;
+                    int option1 = random.nextInt(2);
+                    if (option1 == 0) {
+                        player1Position += rollDice;
+                        if (player1Position > 100) {
+                            player1Position = 100;
+                        }
+                    } else {
+                        player2Position += rollDice;
+                        if (player2Position > 100) {
+                            player2Position = 100;
+                        }
                     }
-                    System.out.println("You got a ladder. Your position is now : " + playerPosition);
+                    System.out.println("Player1 position is now : " + player1Position);
+                    System.out.println("Player2 position is now : " + player2Position);
                     break;
                 case 2:
-                    playerPosition -= rollDice;
-                    if (playerPosition < 0) {
-                        playerPosition = 0;
+                    int option2 = random.nextInt(2);
+                    if (option2 == 0) {
+                        player1Position -= rollDice;
+                        if (player1Position < 0) {
+                            player1Position = 0;
+                        }
+                    } else {
+                        player2Position -= rollDice;
+                        if (player2Position < 0) {
+                            player2Position = 0;
+                        }
                     }
-                    System.out.println("You got a snake. Your position is now : " + playerPosition);
+                    System.out.println("Player1 position is now : " + player1Position);
+                    System.out.println("Player2 position is now : " + player2Position);
                     break;
             }
         }
-        System.out.println("------------------------------------------------------------------");
-        System.out.println("Congratulations, you won the game!!!");
-        System.out.println("------------------------------------------------------------------");
-        System.out.println("Count time : "+count);
+        if (player1Position > player2Position) {
+            System.out.println("------------------------------------------------------------------");
+            System.out.println("Congratulations, Player1 won the game!!!");
+            System.out.println("------------------------------------------------------------------");
+        } else {
+            System.out.println("------------------------------------------------------------------");
+            System.out.println("Congratulations, Player2 won the game!!!");
+            System.out.println("------------------------------------------------------------------");
+        }
+        System.out.println("Count number of time dice was played to win the game is : "+count);
 
     }
 }
